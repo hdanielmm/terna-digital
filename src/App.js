06f7e55c70b30cc058fad20b71e1c6ea3291ba89
ActiveOne/DanielMarín <daniel.marin@activeone.com>
2020-10-31 23:58:19 -0500

    Add navbar and menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Terna Digital</h1>
-    </div>
+    <Router className="App">
+      <Navbar />
+      <Switch>
+        <Route path='/' />
+      </Switch>
+    </Router>
   );
 }
 
